10bb9d3e1a697f6443c9542cf8e9632efedf23f1
James Read <james.read.18@gmail.com>
2026-03-17 21:47:39 +0000

    chore: adding config file

---
+++ docs/conf.py
@@ -0,0 +1,51 @@
+from sphinx.highlighting import lexers
+from pygments.lexers.web import PhpLexer
+
+lexers['php'] = PhpLexer(startinline=True, linenos=1)
+lexers['php-annotations'] = PhpLexer(startinline=True, linenos=1)
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'jwt-auth'
+copyright = '2025, James Read'
+author = 'James Read'
+release = '3.0.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.doctest',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage',
+    'sphinx.ext.imgmath',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.githubpages',
+    'sphinx.ext.napoleon',
+    'sphinx_rtd_theme',
+    'sphinx_multiversion',
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['.venv', 'bin', 'build', 'include', 'lib','lib64']
+
+root_doc = 'index'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+html_theme = 'sphinx_rtd_theme'
+highlight_language = "php"
+
+smv_tag_whitelist = r'^.*$'
+smv_branch_whitelist = r'^(\d\.x|main)$'
+smv_remote_whitelist = r'^.*$'
+smv_released_pattern = r'^tags/.*$'
+smv_outputdir_format = '{ref.name}'
+smv_prefer_remote_refs = False
